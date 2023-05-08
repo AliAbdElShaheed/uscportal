c68889943bae7b2b77a27dcd971fddc762f447b1
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2023-05-08 14:01:51 +0300

    starting work with front home page

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     //return view('welcome');
-    return redirect()->route('dashboard.index');
+    return view('welcome');
 });
 
-Auth::routes(['register'=> false]);
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
