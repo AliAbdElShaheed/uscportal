a5d5eb94140f1571ead1211c1541ec50450cc373
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2023-05-22 14:00:48 +0300

    almost finish home page without some js codes

--- public/frontend_files/js/main.js
+++ public/frontend_files/js/main.js
@@ -15,3 +15,30 @@ $(document).ready(function () {
     });//end of window scroll
 
 });//end of document ready
+
+
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+
+    // Stats Increase Number
+    if (window.scrollY >= statsSection.offsetTop - 300) {
+        // console.log('arrive');
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+}
