dd37aab111df3182f6e054a0367ccd2f37f24da3
USCPortal <130317158+USCPortal@users.noreply.github.com>
2023-09-17 19:36:15 +0300

    modify in footer

--- resources/views/layouts/frontend/_footer.blade.php
+++ resources/views/layouts/frontend/_footer.blade.php
@@ -6,28 +6,28 @@
                     <img src="{{asset('uploads/frontend/images/logo3.png')}}" alt="USC Logo" class="w-50"/>
                     <ul class="d-flex my-3 list-unstyled gap-3 justify-content-sm-center justify-content-md-start">
                         <li>
-                            <a class="d-block text-light" href="#"
+                            <a class="d-block text-light" href="https://www.facebook.com/usc.edu.eg"
                             ><i class="fa-brands fa-facebook fa-lg facebook rounded-circle p-2"></i
                                 ></a>
                         </li>
                         <li>
-                            <a class="d-block text-light" href="#"
+                            <a class="d-block text-light" href="https://twitter.com/Univ_SadatCity"
                             ><i class="fa-brands fa-twitter fa-lg twitter rounded-circle p-2"></i
                                 ></a>
                         </li>
                         <li>
-                            <a class="d-block text-light" href="#"
+                            <a class="d-block text-light" href="https://www.linkedin.com/company/university-of-sadat-city/"
                             ><i class="fa-brands fa-linkedin fa-lg linkedin rounded-circle p-2"></i
                                 ></a>
                         </li>
                         <li>
-                            <a class="d-block text-light" href="#"
+                            <a class="d-block text-light" href="https://www.instagram.com/universityofsadatcity/"
                             ><i class="fa-brands fa-instagram fa-lg instagram rounded-circle p-2"></i
                                 ></a>
                         </li>
 
                         <li>
-                            <a class="d-block text-light" href="#"
+                            <a class="d-block text-light" href="https://www.youtube.com/@U.S.C"
                             ><i class="fa-brands fa-youtube fa-lg youtube rounded-circle p-2"></i
                                 ></a>
                         </li>
@@ -36,7 +36,7 @@
                         Get in touch with us via email or phone.<br>
                         We are waiting for your call or message
                     </p>
-                    <a class="btn rounded-pill main-btn w-75" href="#">Dean@usc.edu.eg</a>
+                <!-- <a class="btn rounded-pill main-btn w-75" href="#">Dean@usc.edu.eg</a>   -->
 
                 </div>
             </div>
@@ -44,12 +44,11 @@
                 <div class="links">
                     <h5 class="text-light">@lang('site.Links') </h5>
                     <ul class="list-unstyled lh-lg">
-                        <li><a href="#">@lang('front.about_university')</a></li>
-                        <li><a href="#">@lang('Management')</a></li>
-                        <li><a href="#">@lang('PGraduate_and_Research')</a></li>
-                        <li><a href="#">@lang('front.colleges_and_institutes')</a></li>
-                        <li><a href="#">@lang('front.electronic_services')</a></li>
-                        <li><a href="#">@lang('Terms and Condition')</a></li>
+                        <li><a href="/about">@lang('front.about_university')</a></li>
+                        <li><a href="/management">@lang('front.university_management')</a></li>
+                        <li><a href="#">@lang('front.postgraduate_and_research')</a></li>
+                        <li><a href="/colleges">@lang('front.colleges_and_institutes')</a></li>
+
                     </ul>
                 </div>
             </div>
@@ -57,11 +56,11 @@
                 <h5 class="text-light mb-3">@lang('site.ContactUs') </h5>
                 <div class="line">
                     <i class="fas fa-map-marker-alt fa-fw"></i>
-                    <div class="info">Sadat University, Abdel Moneim Riad St, Sadat City, Egypt</div>
+                    <div class="info">@lang('site.USCInfo')</div>
                 </div>
                 <div class="line">
                     <i class="far fa-clock fa-fw"></i>
-                    <div class="info">Business Hours: From 09:00 To 17:00</div>
+                    <div class="info">@lang('site.BusinessHours')</div>
                 </div>
                 <div class="line">
                     <i class="fas fa-phone-volume fa-fw"></i>
