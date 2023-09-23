ffc83741af18f6574e609b111b83cfe4799401b2
Ali Mohammed <engineer.ali.mohammed@fcai.usc.edu.eg>
2023-09-23 13:47:54 +0300

    back to top arrow

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -8,12 +8,17 @@
     {{--------------------------End of nav bar----------------------------------------------------}}
 
 
+    {{--------------------------Start of Scroll To Top Button ------------------------------------}}
+    <span class="up"><i class="fas fa-angle-double-up"></i></span>
+    {{--<span class="up"><i class="fas fa-caret-up"></i></span>--}}
+    {{--------------------------End of Scroll To Top Button --------------------------------------}}
+
 
     @yield('content')
 
 
 
-    
+
     {{-----------------------------Start of Footer ----------------------------------------------}}
     @include('layouts.frontend._footer')
     {{--------------------------   End of Footer ------------------------------------------------}}
@@ -28,5 +33,6 @@
 @stack('scripts')
 </body>
 </html>
+{{--// Script to Accessibility Plugin--}}
 <script src="https://website-widgets.pages.dev/dist/sienna.min.js" defer></script>
 
